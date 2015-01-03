f8bbb47c5c3e705a263d07c239eb1cf1d2900d1f
b25vX3Bt <b25vx3bt@gmail.com>
2015-01-03 20:30:57 +0900

    update goroutine

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -5,27 +5,60 @@ import (
 	"time"
 )
 
-func enqueue(q chan int) {
+func enqueue_1(q chan int) {
 	for i := 0; i < 100; i++ {
-		fmt.Printf("queue cap[%d] len[%d]\n", cap(q), len(q))
+        fmt.Printf("queue info: cap[%d] len[%d]\n", cap(q), len(q))
+
+        fmt.Printf("enqueue:[%d]\n",i)
+		q <- i // send i to channel
 
 		if len(q) > 100 {
 			time.Sleep(100 * time.Millisecond)
-		}
+		} else {
 
-		q <- i // send i to channel
+            if i%2 == 0 {
+			    time.Sleep(1000 * time.Millisecond)
+            } else {
+			    time.Sleep(2000 * time.Millisecond)
+            }
+        }
 	}
 }
-
-func main() {
-
+func dequeue_1() {
 	queue := make(chan int, 10)
 
-	go enqueue(queue)
+	go enqueue_1(queue)
 
 	for i := 0; i < 5; i++ {
 		val := <-queue // init val and recv from channel
 		fmt.Printf("dequeue:[%d]\n", val)
 	}
-
 }
+
+func enqueue_2(q chan int, i int){
+    if i%2 == 0 {
+        time.Sleep(1000 * time.Millisecond)
+    }else{
+        time.Sleep(5000 * time.Millisecond)
+    }
+    q <- i
+}
+func dequeue_2() {
+    queue := make(chan int, 100)
+
+    for i := 0; i < 5; i++ {
+        go enqueue_2(queue,i)
+    }
+	for i := 0; i < 5; i++ {
+		val := <-queue // init val and recv from channel
+		fmt.Printf("dequeued val:[%d]\n", val)
+	}
+}
+
+func main() {
+
+    //dequeue_1()
+
+    dequeue_2()
+}
+
